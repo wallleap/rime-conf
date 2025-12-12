66d16f4036eb5cdf343f1826ab0e4cc5bad931e0
wallleap <15579576761@163.com>
2025-12-12 12:50:39 +0800

    perf: 实现日期翻译模块的按需加载以提升启动性能
    延迟加载日期翻译模块，减少启动时的资源消耗

--- Rime.lua
+++ Rime.lua
@@ -1,3 +1,16 @@
 -- date_translator: 动态日期时间输入
 -- 文件在 `lua/date_translator.lua`
-date_translator = require("date_translator")
+-- 按需加载机制，延迟加载模块以提升启动性能
+local date_translator_module = nil
+
+local function load_date_translator()
+    if not date_translator_module then
+        date_translator_module = require("date_translator")
+    end
+    return date_translator_module
+end
+
+function date_translator(input, seg)
+    local translator = load_date_translator()
+    return translator(input, seg)
+end
